f50f2110c65267ed13b7e3828d4c47e3b4b89751
unknown <kosletr@gmail.com>
2019-11-24 22:40:21 +0200

    Initial Commit

---
+++ GitInsightsDB.sql
@@ -0,0 +1,296 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gitinsightsdb
+-- ------------------------------------------------------
+-- Server version	5.5.57-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `commit`
+--
+
+DROP TABLE IF EXISTS `commit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `commit` (
+  `hash` binary(20) NOT NULL,
+  `repo_id` bigint(20) NOT NULL,
+  `timestamp` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` bigint(20) NOT NULL,
+  `branch` varchar(50) NOT NULL,
+  PRIMARY KEY (`hash`),
+  KEY `userID_idx` (`user_id`),
+  KEY `repoID_idx` (`repo_id`),
+  CONSTRAINT `userID` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `repoID` FOREIGN KEY (`repo_id`) REFERENCES `repo` (`repo_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commit`
+--
+
+LOCK TABLES `commit` WRITE;
+/*!40000 ALTER TABLE `commit` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commit` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `language - repo`
+--
+
+DROP TABLE IF EXISTS `language - repo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `language - repo` (
+  `language` varchar(50) NOT NULL,
+  `lines_of_code` int(11) NOT NULL,
+  `repo_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`language`),
+  KEY `repoID_idx` (`repo_id`),
+  CONSTRAINT `IDrepo` FOREIGN KEY (`repo_id`) REFERENCES `repo` (`repo_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `languageName` FOREIGN KEY (`language`) REFERENCES `programming language` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `language - repo`
+--
+
+LOCK TABLES `language - repo` WRITE;
+/*!40000 ALTER TABLE `language - repo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `language - repo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `organization`
+--
+
+DROP TABLE IF EXISTS `organization`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `organization` (
+  `name` varchar(50) NOT NULL,
+  `description` text,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `organization`
+--
+
+LOCK TABLES `organization` WRITE;
+/*!40000 ALTER TABLE `organization` DISABLE KEYS */;
+/*!40000 ALTER TABLE `organization` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `organization - user`
+--
+
+DROP TABLE IF EXISTS `organization - user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `organization - user` (
+  `org_name` varchar(50) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`org_name`,`user_id`),
+  KEY `ID_user_idx` (`user_id`),
+  CONSTRAINT `ID_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `org_name` FOREIGN KEY (`org_name`) REFERENCES `organization` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `organization - user`
+--
+
+LOCK TABLES `organization - user` WRITE;
+/*!40000 ALTER TABLE `organization - user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `organization - user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `owner`
+--
+
+DROP TABLE IF EXISTS `owner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `owner` (
+  `owner_id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL,
+  `owner_type` enum('User','Team') NOT NULL,
+  PRIMARY KEY (`owner_id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `owner_type_UNIQUE` (`owner_type`),
+  CONSTRAINT `idUser` FOREIGN KEY (`id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `idTeam` FOREIGN KEY (`id`) REFERENCES `team` (`team_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `owner`
+--
+
+LOCK TABLES `owner` WRITE;
+/*!40000 ALTER TABLE `owner` DISABLE KEYS */;
+/*!40000 ALTER TABLE `owner` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `programming language`
+--
+
+DROP TABLE IF EXISTS `programming language`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `programming language` (
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `programming language`
+--
+
+LOCK TABLES `programming language` WRITE;
+/*!40000 ALTER TABLE `programming language` DISABLE KEYS */;
+/*!40000 ALTER TABLE `programming language` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `repo`
+--
+
+DROP TABLE IF EXISTS `repo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `repo` (
+  `name` varchar(50) NOT NULL,
+  `owner_id` bigint(20) NOT NULL,
+  `repo_id` bigint(20) NOT NULL,
+  `stars` int(11) NOT NULL,
+  `forks` int(11) NOT NULL,
+  `is_public` tinyint(1) NOT NULL,
+  PRIMARY KEY (`repo_id`),
+  UNIQUE KEY `owner_id_UNIQUE` (`owner_id`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  CONSTRAINT `owner` FOREIGN KEY (`owner_id`) REFERENCES `owner` (`owner_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `repo`
+--
+
+LOCK TABLES `repo` WRITE;
+/*!40000 ALTER TABLE `repo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `repo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `team`
+--
+
+DROP TABLE IF EXISTS `team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `team` (
+  `name` varchar(50) NOT NULL,
+  `organization` varchar(50) NOT NULL,
+  `description` text,
+  `team_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`team_id`),
+  UNIQUE KEY `organization_UNIQUE` (`organization`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  CONSTRAINT `organization` FOREIGN KEY (`organization`) REFERENCES `organization` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `team`
+--
+
+LOCK TABLES `team` WRITE;
+/*!40000 ALTER TABLE `team` DISABLE KEYS */;
+/*!40000 ALTER TABLE `team` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `username` varchar(50) NOT NULL,
+  `fullname` varchar(50) DEFAULT NULL,
+  `date_of_birth` date NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `date_of_birth_UNIQUE` (`date_of_birth`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user-team`
+--
+
+DROP TABLE IF EXISTS `user-team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user-team` (
+  `team_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`team_id`,`user_id`),
+  KEY `id_user_idx` (`user_id`),
+  CONSTRAINT `id_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_team_fk` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user-team`
+--
+
+LOCK TABLES `user-team` WRITE;
+/*!40000 ALTER TABLE `user-team` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user-team` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-23 16:43:10
