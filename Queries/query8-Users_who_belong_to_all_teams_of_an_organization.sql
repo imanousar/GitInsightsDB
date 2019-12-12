0df38f0a5e9bdd706004aa0238d418260be6e90d
Giannis <giannismanu97@gmail.com>
2019-12-12 13:22:21 +0200

    Add query8

---
+++ Queries/query8-Users_who_belong_to_all_teams_of_an_organization.sql
@@ -0,0 +1,38 @@
+# Users who belong to all teams of an organization. Apply for Facebook.
+
+# Table A
+create table a as select
+organization.id, team.name
+from team 
+join organization on team.org_id = organization.id
+where organization.name = "Facebook" ;
+
+# Table B
+create table b as select
+a.id, a.name, user_team.user_id
+from a
+join user_team on (a.name = user_team.team_name and a.id = user_team.org_id)
+;
+
+# Table t = B/A
+create temporary table t as select distinct x.user_id
+from b as x
+where not exists (
+                  select *
+                  from  a AS y
+                  where not exists (
+                                     select *
+                                     from b as z
+                                     where (z.user_id=x.user_id) and (z.name=y.name)
+                                   )
+                 );
+
+# Final Result
+select username,fullname
+from user 
+join t on user.id = t.user_id;
+
+# Drop Tables
+drop table a;
+drop table b;
+drop table t;
