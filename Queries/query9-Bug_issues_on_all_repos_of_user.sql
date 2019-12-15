dff27e2682dcdc0f923e1bf79da0c8cd1d25f3d7
Alexandros Sahinis <alexsah97@gmail.com>
2019-12-15 12:59:49 +0200

    Add 2 queries

---
+++ Queries/query9-Bug_issues_on_all_repos_of_user.sql
@@ -0,0 +1,10 @@
+# Apply for user konsletr
+SELECT issue.*, repo.name, user.username
+FROM 
+    repo 
+        JOIN 
+    user on repo.owner_id = user.id
+        JOIN 
+    issue on repo.id = issue.repo_id
+WHERE
+    user.username = "konsletr" and issue.label = "bug";
