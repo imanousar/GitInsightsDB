c7618c62a51633ec348b465f90776b121a9686c3
imanousar <giannismanu97@gmail.com>
2020-01-01 22:48:12 +0200

    Add repos

--- gitinsights/gitinsights/settings.py
+++ gitinsights/gitinsights/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gitinsightsdb',
         'USER': 'root',
-        'PASSWORD': '1997',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
