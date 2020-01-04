4b754dcecf0119b1e020c076bc27ea558596793b
imanousar <giannismanu97@gmail.com>
2020-01-04 12:41:42 +0200

    change users html

--- gitinsights/gitinsights/settings.py
+++ gitinsights/gitinsights/settings.py
@@ -77,12 +77,11 @@ WSGI_APPLICATION = 'gitinsights.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT']
-    }
+              'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASSWORD'],
+            'HOST': os.environ['DB_HOST'],
+            'PORT': os.environ['DB_PORT']
 }
 
 # Password validation
